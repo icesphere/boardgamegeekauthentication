5d7face2077ce95b5be22afba71d36c894acd194
John <icesphere12@gmail.com>
2014-01-02 21:11:34 -0700

    add back init to custom realm

--- src/main/java/org/smartreaction/boardgamegeek/auth/CustomRealm.java
+++ src/main/java/org/smartreaction/boardgamegeek/auth/CustomRealm.java
@@ -1,16 +1,37 @@
 package org.smartreaction.boardgamegeek.auth;
 
 import com.sun.appserv.security.AppservRealm;
+import com.sun.enterprise.security.auth.realm.BadRealmException;
 import com.sun.enterprise.security.auth.realm.InvalidOperationException;
+import com.sun.enterprise.security.auth.realm.NoSuchRealmException;
 import com.sun.enterprise.security.auth.realm.NoSuchUserException;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CustomRealm extends AppservRealm
 {
+    public static final String PARAM_JAAS_CONTEXT = "jaas-context";
+
+    @Override
+    public void init(Properties properties) throws BadRealmException, NoSuchRealmException
+    {
+        String propJaasContext = properties.getProperty(PARAM_JAAS_CONTEXT);
+
+        if (propJaasContext != null) setProperty(PARAM_JAAS_CONTEXT, propJaasContext);
+
+        Logger logger = Logger.getLogger(this.getClass().getName());
+
+        String realmName = this.getClass().getSimpleName();
+
+        logger.log(Level.INFO, realmName + " started. ");
+        logger.log(Level.INFO, realmName + ": " + getAuthType());
+        logger.log(Level.INFO, realmName + " authentication uses jar file in $domain/lib folder ");
+
+        for (Map.Entry<Object, Object> property:properties.entrySet()) logger.log(Level.INFO, property.getKey() + ": " + property.getValue());
+    }
+
     @Override
     public String getAuthType()
     {
